5cc5799bc47039eb122b85fb9e37b3a7adb7b456
Gyandeep Aryan <gyandeeparyan2@gmail.com>
2023-10-12 23:16:00 +0530

    fix:signin page updated

--- src/Pages/Signin.jsx
+++ src/Pages/Signin.jsx
@@ -2,26 +2,33 @@
 import { Button } from "../../src/components/index";
 import { signupContent } from "../utils/constants";
 import Check from '../../public/assets/Check.svg'
+import LogoBlack from '../../public/assets/760_rectangle.svg'
 const Signin = () => {
   return (
     <div className='container flex h-screen'>
       <div className='w-1/2 bg-brand-yellow-muted  h-[800px]'>
-       <h1 className="text-brand-coal text-4xl mt-7 mb-4 font-bold text-center">{signupContent?.heading}</h1>
+       <h1 className="text-brand-dark-gray text-4xl mt-7 mb-8 font-bold text-center">{signupContent?.heading}</h1>
      
-      <p className="text-brand-coal mb-2 px-4 text-sm">{signupContent?.description}</p>
-      <p className="text-brand-coal  mb-6 px-4 text-sm">{signupContent?.descriptionSecond}</p>
-    <div className="flex flex-col">
-
-    </div>
+      <p className="text-brand-coal mb-2 px-10 text-sm">{signupContent?.description}</p>
+      <p className="text-brand-coal  mb-6 px-10 text-sm">{signupContent?.descriptionSecond}</p>
+   
      {signupContent?.features.map((feature)=>{return(
       <>
-      <div className="px-4">
-      <img src={Check} alt="check" className="inline mr-2" />
-<p className="text-brand-coal inline text-lg font-semibold">{feature}</p>
+      <div className="px-14 ">
+        <div className="inline-block">
+        <img src={Check} alt="check" className="inline mr-2" />
+        </div>
+
+<p className="text-brand-dark-gray px-4 text-left inline text-md font-bold">{feature}</p>
       </div>
+
+      
 
       </>
      )})}
+     <div>
+        <img src={LogoBlack}className="mt-10 ml-[200px]" alt={"logo-black"} />
+      </div>
       </div>
       <div className='w-1/2 h-screen  '>
         <h1 className='text-brand-coal mt-8 ml-[200px] text-2xl'>Enterprise Plan Request</h1>
